a658ff6cc6e18fbe131c4cb073452e5beb4833cc
meipey <82648623+meipey@users.noreply.github.com>
2022-03-15 11:04:02 +0100

    Batch update

--- js/script.js
+++ js/script.js
@@ -1,4 +1,25 @@
 
 
     window.silex = window.silex || {}
-    window.silex.data = {"site":{"width":1200},"pages":[{"id":"page-default","displayName":"Default","link":{"linkType":"LinkTypePage","href":"#!page-default"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false},{"id":"page-home","displayName":"Home","link":{"linkType":"LinkTypePage","href":"#!page-home"},"canDelete":true,"canRename":true,"canMove":true,"canProperties":true,"opened":false},{"id":"page-404","displayName":"404","link":{"linkType":"LinkTypePage","href":"#!page-404"},"canDelete":true,"canRename":true,"canMove":true,"canProperties":true,"opened":false},{"id":"page-products","displayName":"Products","link":{"linkType":"LinkTypePage","href":"#!page-products"},"canDelete":true,"canRename":true,"canMove":true,"canProperties":true,"opened":false},{"id":"page-product","displayName":"Product","link":{"linkType":"LinkTypePage","href":"#!page-product"},"canDelete":true,"canRename":true,"canMove":true,"canProperties":true,"opened":false},{"id":"page-post","displayName":"Post","link":{"linkType":"LinkTypePage","href":"#!page-post"},"canDelete":true,"canRename":true,"canMove":true,"canProperties":true,"opened":false},{"id":"page-tags","displayName":"Tags","link":{"linkType":"LinkTypePage","href":"#!page-tags"},"canDelete":true,"canRename":true,"canMove":true,"canProperties":true,"opened":false},{"id":"page-tag","displayName":"Tag","link":{"linkType":"LinkTypePage","href":"#!page-tag"},"canDelete":true,"canRename":true,"canMove":true,"canProperties":true,"opened":false}]}
+    window.silex.data = {"site":{"width":1000},"pages":[{"id":"page-home-page","displayName":"Home Page","link":{"linkType":"LinkTypePage","href":"#!page-home-page"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false},{"id":"page-product-details","displayName":"Product Details","link":{"linkType":"LinkTypePage","href":"#!page-product-details"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false},{"id":"page-about","displayName":"About","link":{"linkType":"LinkTypePage","href":"#!page-about"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false}]}
+
+        $(function() {
+            /* hamburger menu */
+            $('.close-btn, .open-btn').click(function() {
+                $('body').toggleClass('is-not-active is-active');
+            });
+
+            /*
+                animation widget from here: https://github.com/silexlabs/Silex/issues/443
+            */
+
+            window.sr = ScrollReveal({
+                distance: '100px'
+            });
+            sr.reveal('.from-left', { origin:  'left'});
+            sr.reveal('.from-right', { origin:  'right'});
+            sr.reveal('.from-top', { origin:  'top'});
+            sr.reveal('.from-bottom', { origin:  'bottom'});
+
+        })
+    
